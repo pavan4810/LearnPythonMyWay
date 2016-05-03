22f09ffd86b663a34829596765974e23ac3e3e6a
Pavan Kumar Avala <pavan4810@gmail.com>
2016-05-02 21:16:35 -0400

    String join operator

---
+++ strings/strings_join.py
@@ -0,0 +1,38 @@
+"""
+Notes: =========================================================================
+Notes: separator_character.join(list of words)
+Notes:   Ex:  ' '.join(input), ':'.join(input), '/'.join(input)
+Notes:
+"""
+
+# Input list
+input_list = ["Pavan", "Kumar", "Avala", "is", "Great"]
+
+# join using ' ' as separator
+stuff = ' '.join(input_list)
+print stuff
+
+# join using ':' as separator
+stuff = ':'.join(input_list)
+print stuff
+
+# join using '/' as separator
+stuff = '/'.join(input_list)
+print stuff
+
+
+
+# Input list
+input_list = ['P', 'a', 'v', 'a', 'n']
+
+# join using ' ' as separator
+stuff = ' '.join(input_list)
+print stuff
+
+# Input list
+input_list = ['P', 'a', 'v', 'a', 'n']
+
+# join using '' as separator
+stuff = ''.join(input_list)
+print stuff
+
