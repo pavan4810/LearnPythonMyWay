5d3a7e331d10dede8a50b9562817ee3c800ec3dc
Pavan Kumar Avala <pavan4810@gmail.com>
2016-05-13 10:22:33 -0400

    List operations

--- lists/lists_operations.py
+++ lists/lists_operations.py
@@ -1,11 +1,48 @@
 """
 Notes: =========================================================================
+Notes: len(list)
+Notes:   This function gives size of list.
+Notes:
+Notes: list.append(list element)
+Notes:   This function appends elements to list.
+Notes:   This is equal to    a[len(a):] = [x]
+Notes:
+Notes: list.extend(list2)
+Notes:   Extend the list by appending all the items in the given list;
+Notes:   equivalent to       a[len(a):] = L
+Notes:
 Notes: list.pop(index)
 Notes:   This function pops an element from the list.
 Notes:   Ex: list.pop(0), list.pop(1), list.pop(-1) list.pop(-2)
 Notes:
 """
 
+#===============================================================================
+name = ["Pavan", "Kumar", "Avala"]
+print "List : ", name
+print "List length : ", len(name)
+print ""
+
+
+#===============================================================================
+name = ["Pavan", "Kumar", "Avala"]
+print "List before appending : ", name
+name.append("The")
+name[len(name):] = ["Great"]
+print "List after  appending : ", name
+print ""
+
+
+#===============================================================================
+name = ["Pavan", "Kumar", "Avala"]
+print "List before extending : ", name
+extention = ["The", "Great"]
+name.extend(extention)
+print "List after  extending : ", name
+print ""
+
+
+#===============================================================================
 def pop_a_word(words, index):
     """ This function pops a word from specified index."""
     return words.pop(index)
