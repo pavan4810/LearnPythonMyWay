253b3c7cb13263daa928301d96827a9c3dc0330a
Pavan Kumar Avala <pavan4810@gmail.com>
2016-05-24 19:47:47 -0400

    thread join and mutex lock

--- threads/threads1.py
+++ threads/threads1.py
@@ -4,30 +4,43 @@ Notes: Threading.
 Notes:      Thread(target=<target method>, args=(arg1, arg2, ...))
 Notes:
 Notes: t.start() to start the thread.
+Notes: t.join() makes the caller thread to wait until the callee thread exits.
+Notes:
+Notes: tl.acquire() blocks until it acquire the lock.
+Notes: tl.release() releases the lock, if acquired.
 Notes:
 Notes: time.time() gives current time.
 Notes: time.ctime() converts given time into ascii format.
 Notes:
 """
 
-from threading import Thread
+import threading
 import time
+
+tLock = threading.Lock()
 
 def timer(name, delay, repeat):
     print "Timer: %s started" %(name)
     while repeat > 0:
         time.sleep(delay)
+        tLock.acquire()
+        print name," has acquired the lock"
         print "%s : %s" %(name, time.ctime(time.time()))
+        tLock.release()
+        print name," has released the lock"
         repeat -= 1
     print "Timer: %s stopped" %(name)
 
+
 def Main():
-    t1 = Thread(target=timer, args=("Timer1", 1, 5))
-    t2 = Thread(target=timer, args=("Timer2", 2, 5))
+    t1 = threading.Thread(target=timer, args=("Timer1", 1, 5))
+    t2 = threading.Thread(target=timer, args=("Timer2", 2, 5))
 
     t1.start()
     t2.start()
 
+    # Wait for thread1 to complete
+    t1.join()
     print "Main completed"
 
 
