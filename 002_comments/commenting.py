30f3cae2c0a4a788e0119f00ead1c8887f948b4c
Pavan Kumar Avala <pavan.picasa@gmail.com>
2016-08-16 15:15:52 -0400

    Sort and clean up

---
+++ 002_comments/commenting.py
@@ -0,0 +1,15 @@
+"""
+Notes: =========================================================================
+Notes: Commenting:
+Notes:     1) use #
+Notes:     2) use 3 double quotes in a seqence to begin and to end
+Notes:     3) 3 double quotes after print is considered as multi-line printing.
+"""
+
+print "# First way of commenting code"
+
+print """
+\"\"\"
+ Second way of commenting in code.
+\"\"\"
+"""
